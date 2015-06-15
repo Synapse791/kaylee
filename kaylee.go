78fa363d04e631b7a3008f4df8c991744cd91d9e
Iain Earl <iain@blurgroup.com>
2015-06-15 11:20:42 +0100

    Added help text and improved flags

--- kaylee.go
+++ kaylee.go
@@ -8,14 +8,22 @@ import (
 
 var (
 	configString    string
+	printExample    bool
+	printUsage      bool
 	verbose         bool
 )
 
 func main() {
 
-	flag.StringVar(&configString, "c", "not_set", "JSON string containing config")
+	flag.StringVar(&configString, "config", "not_set", "JSON string containing config")
+	flag.BoolVar(&printExample, "example", false, "print example json")
+	flag.BoolVar(&printUsage, "help", false, "print usage")
 	flag.BoolVar(&verbose, "v", false, "enable verbose output")
 	flag.Parse()
+
+	if printExample { PrintExample() }
+
+	if printUsage || configString == "not_set" { PrintUsage() }
 
 	config, err := GetConfig(configString)
 	if err != nil {
@@ -27,6 +35,50 @@ func main() {
 		LogError(err.Error())
 	}
 
+	os.Exit(0)
+
+}
+
+func PrintUsage() {
+	helpText := `Kaylee - find and replace inside files using json config
+
+  Pass in a JSON string to specify a group of files. Each of these files has a group of find and replace values.
+  Kaylee runs through these groups and replaces the find strings with the replace strings, then moves onto the next file.
+
+`
+
+	fmt.Print(helpText)
+	flag.PrintDefaults()
+	os.Exit(0)
+}
+
+func PrintExample() {
+	example := `[
+  {
+    "path"     : "/tmp/file1",
+    "patterns" : [
+      {
+        "find"    : "FINDME1",
+        "replace" : "am"
+      },
+      {
+        "find"    : "FINDME2",
+        "replace" : "password"
+      }
+    ]
+  },
+  {
+    "path"     : "/tmp/file2",
+    "patterns" : [
+      {
+        "find"    : "FINDME1",
+        "replace" : "put me in the file"
+      }
+    ]
+  }
+]`
+
+	fmt.Print(example)
 	os.Exit(0)
 
 }
