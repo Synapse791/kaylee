4190fc6890a211492ea3c86525fb5f81c1b363aa
Iain Earl <iain@blurgroup.com>
2015-07-09 10:26:11 +0100

    gofmt

--- kaylee.go
+++ kaylee.go
@@ -7,42 +7,46 @@ import (
 )
 
 var (
-	configString    string
-	printExample    bool
-	printUsage      bool
-	verbose         bool
+	configString string
+	printExample bool
+	printUsage   bool
+	verbose      bool
 )
 
 func init() {
-    flag.StringVar(&configString, "c", "not_set", "JSON string containing config")
-    flag.StringVar(&configString, "config", "not_set", "JSON string containing config")
+	flag.StringVar(&configString, "c", "not_set", "JSON string containing config")
+	flag.StringVar(&configString, "config", "not_set", "JSON string containing config")
 
-    flag.BoolVar(&printExample, "e", false, "print example json")
-    flag.BoolVar(&printExample, "example", false, "print example json")
+	flag.BoolVar(&printExample, "e", false, "print example json")
+	flag.BoolVar(&printExample, "example", false, "print example json")
 
-    flag.BoolVar(&printUsage, "h", false, "print usage")
-    flag.BoolVar(&printUsage, "help", false, "print usage")
+	flag.BoolVar(&printUsage, "h", false, "print usage")
+	flag.BoolVar(&printUsage, "help", false, "print usage")
 
-    flag.BoolVar(&verbose, "v", false, "enable verbose output")
-    flag.BoolVar(&verbose, "verbose", false, "enable verbose output")
+	flag.BoolVar(&verbose, "v", false, "enable verbose output")
+	flag.BoolVar(&verbose, "verbose", false, "enable verbose output")
 }
 
 func main() {
 
 	flag.Parse()
 
-	if printExample { PrintExample() }
+	if printExample {
+		PrintExample()
+	}
 
-	if printUsage { PrintUsage() }
+	if printUsage {
+		PrintUsage()
+	}
 
-    if configString == "not_set" {
-        LogError("config [-c|-config] flag is required")
-        PrintUsage()
-    }
+	if configString == "not_set" {
+		LogError("config [-c|-config] flag is required")
+		PrintUsage()
+	}
 
-    LogVerbose("verbose mode enabled")
+	LogVerbose("verbose mode enabled")
 
-    config, err := GetConfig(configString)
+	config, err := GetConfig(configString)
 	if err != nil {
 		LogError(err.Error())
 	}
@@ -91,19 +95,19 @@ func PrintExample() {
 
 }
 
-func LogError(err string, args... interface{}) {
-    if len(args) > 0 {
-        err = fmt.Sprintf(err, args...)
-    }
+func LogError(err string, args ...interface{}) {
+	if len(args) > 0 {
+		err = fmt.Sprintf(err, args...)
+	}
 	fmt.Fprintf(os.Stderr, "\033[01;31mKAYLE_ERROR: %s\033[00;00m\n", err)
 	os.Exit(1)
 }
 
-func LogVerbose(line string, args... interface{}) {
+func LogVerbose(line string, args ...interface{}) {
 	if verbose == true {
-        if len(args) > 0 {
-            line = fmt.Sprintf(line, args...)
-        }
+		if len(args) > 0 {
+			line = fmt.Sprintf(line, args...)
+		}
 		fmt.Printf("verbose: %s\n", line)
 	}
 }
--- replace.go
+++ replace.go
@@ -1,53 +1,53 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"strings"
-	"os"
+    "fmt"
+    "io/ioutil"
+    "os"
+    "strings"
 )
 
 func FindReplace(config KayleeConfig) error {
 
-	for _, file := range config.Files {
-		if _, err := os.Stat(file.Path); os.IsNotExist(err) {
+    for _, file := range config.Files {
+        if _, err := os.Stat(file.Path); os.IsNotExist(err) {
             LogVerbose(err.Error())
-			return fmt.Errorf("file %s does not exist", file.Path)
-		}
-	}
+            return fmt.Errorf("file %s does not exist", file.Path)
+        }
+    }
 
-	for _, file := range config.Files {
-		LogVerbose("configuring %s", file.Path)
+    for _, file := range config.Files {
+        LogVerbose("configuring %s", file.Path)
 
-		data, err := ioutil.ReadFile(file.Path)
-		if err != nil {
+        data, err := ioutil.ReadFile(file.Path)
+        if err != nil {
             LogVerbose(err.Error())
-			return fmt.Errorf("failed to read file %s", file.Path)
-		}
+            return fmt.Errorf("failed to read file %s", file.Path)
+        }
 
-		newContents := string(data)
+        newContents := string(data)
 
-		for find, replace := range file.Patterns {
-			if strings.Contains(newContents, find) {
-				LogVerbose("replacing '%s' with '%s' in %s", find, replace, file.Path)
-				newContents = strings.Replace(newContents, find, replace, -1)
-			} else {
-				return fmt.Errorf("'%s' not found in '%s'", find, file.Path)
-			}
+        for find, replace := range file.Patterns {
+            if strings.Contains(newContents, find) {
+                LogVerbose("replacing '%s' with '%s' in %s", find, replace, file.Path)
+                newContents = strings.Replace(newContents, find, replace, -1)
+            } else {
+                return fmt.Errorf("'%s' not found in '%s'", find, file.Path)
+            }
 
-		}
+        }
 
-		LogVerbose("writing file %s", file.Path)
-		err = ioutil.WriteFile(file.Path, []byte(newContents), 0)
-		if err != nil {
+        LogVerbose("writing file %s", file.Path)
+        err = ioutil.WriteFile(file.Path, []byte(newContents), 0)
+        if err != nil {
             LogVerbose(err.Error())
-			return fmt.Errorf("failed to write file %s", file.Path)
-		}
+            return fmt.Errorf("failed to write file %s", file.Path)
+        }
 
-		LogVerbose("successfully configured %s", file.Path)
+        LogVerbose("successfully configured %s", file.Path)
 
-	}
+    }
 
-	return nil
+    return nil
 
 }
